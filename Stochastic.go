ca3148e287ef5e6db62e023c43b859a0313487cd
Gökhan Koçak <gokhan.kocak@mail.ru>
2018-09-09 16:09:53 +0300

    License Update

--- Stochastic.go
+++ Stochastic.go
@@ -1,16 +1,36 @@
+/*
+Copyright 2018 www.gokhankocak.com gokhan.kocak@mail.ru
+
+The MIT License
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+/*
+    Implements Stochastic Technical Analysis
+    Stochastic Slow
+*/
+
 package stoploss
 
 import(
   "math"
 )
 
-type Stocastic struct {
+type Stochastic struct {
   DataSet Series
   DebugMode bool
 }
 
-func (DataSet Series) NewStocastic() *Stocastic {
-  st := new(Stocastic)
+func (DataSet Series) NewStochastic() *Stochastic {
+  st := new(Stochastic)
   st.DataSet = DataSet
   return st
 }
@@ -30,7 +50,7 @@ Highest High = highest high for the look-back period
 // PeriodK = 3
 // PeriodD = 2
 
-func (st *Stocastic) Slow( Period int, PeriodK int, PeriodD int ) (float64, float64) {
+func (st *Stochastic) Slow( Period int, PeriodK int, PeriodD int ) (float64, float64) {
 
 	var Highest float64
 	var Lowest float64
